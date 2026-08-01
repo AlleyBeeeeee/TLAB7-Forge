f7a3f9fb36f038d99cb9fc8fb776c1f83150578d
Alexandra Blandon <Alleyb.cruz@gmail.com>
2026-07-31 21:02:39 -0400

    Initial capstone forge pipeline

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from personal host IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["68.237.55.102/32"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0cd
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
